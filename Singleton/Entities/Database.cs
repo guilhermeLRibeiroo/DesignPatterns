546d17bf71019c6f1fd15ff86dcb26dcab9108dc
Guilherme Ribeiro <guilazribeiro@gmail.com>
2022-08-04 20:35:00 -0300

    Added Singleton Pattern

---
+++ Singleton/Entities/Database.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton.Entities
+{
+    class Database
+    {
+        private static readonly object _lock = new object();
+        private static Database _instance;
+
+        private Database() { }
+
+        public static Database GetInstance(string value)
+        {
+            lock (_lock)
+            {
+                // Prevent threads to reach this part of code at the same time
+                if (_instance == null)
+                {
+                    _instance = new Database();
+                    _instance.Value = value;
+                }
+            }
+
+            return _instance;
+        }
+
+        // This variable is meant to test if more than one thread reach the 17th line of code.
+        public string Value { get; set; }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using Singleton.Entities;
+
+Console.WriteLine("If both values are equal it means two threads initialized the singleton at the same time.");
+
+Thread process1 = new Thread(() =>
+{
+    TestSingleton("Process 1");
+});
+
+Thread process2 = new Thread(() =>
+{
+    TestSingleton("Process 2");
+});
+
+process1.Start();
+process2.Start();
+
+process1.Join();
+process2.Join();
+
+void TestSingleton(string value)
+{
+    var databaseSingleton = Database.GetInstance(value);
+    Console.WriteLine("({0}) Value: '{1}'", value, databaseSingleton.Value);
+}
