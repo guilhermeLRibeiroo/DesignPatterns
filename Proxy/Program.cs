8354b0ce8aa13f782f766b00c01f9eb405ac001d
Guilherme Lazari Ribeiro <guilazribeiro@gmail.com>
2022-12-28 18:58:27 -0300

    Added comments to "Proxy" pattern

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,14 +1,16 @@
 ﻿using Proxy.Entities;
 using Proxy.Interfaces;
 
+// "Provides a surrogate or placeholder for another object to control access to it"..
+
 RealSubject realSubject = new RealSubject();
 ClientCode(realSubject);
 
 Console.WriteLine();
 
+// Instead of calling RealSubject directly, you call the proxy that calls the RealSubject.
 SubjectProxy subjectProxy = new SubjectProxy();
 ClientCode(subjectProxy);
-
 
 void ClientCode(ISubject subject)
 {
